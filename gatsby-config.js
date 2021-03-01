d4eb350b1dc0a105cf86b1e377a4d8b858019548
Doni Rubiagatra <doni@zero-one-group.com>
2021-03-01 10:01:28 +0700

    add some css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "React For Designer"
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: ["gatsby-plugin-react-helmet"]
+};
