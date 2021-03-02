004474a326fe539d3ca9098d19f7f1eca664c926
Doni Rubiagatra <doni@zero-one-group.com>
2021-03-02 07:22:16 +0700

    Add Hero and header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import Header from '../components/header'
 import './index.css'
 
 const Layout = ({ children, data }) => (
@@ -13,6 +14,7 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
+    <Header />
     {children()}
   </div>
 )
